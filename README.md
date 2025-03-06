070695307e0b01c3ffe4ad1fdf8ba47cb3215d6d
Amir <58517467+tenticlemaster@users.noreply.github.com>
2025-03-05 19:17:52 -0500

    Basic ground work is laid out
    * vite react project created
    
    * fanta.css imported and css copied
    
    * Function components skeleton are created and laid out
    
    * Hero.jsx finished

---
+++ README.md
@@ -0,0 +1,15 @@
+# Info
+This is a react app that lets users track caffeine intake and current concentration in the system. 
+
+Additional app funtions:
+
+* User authentication
+* Cloud history storage
+
+
+
+App uses fanta.css and pre-formated css from [James](https://github.com/jamezmca)
+
+| random | table   |
+| :----: | :----:  |
+| ass    | muncher |
---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import CoffeeForm from "./components/CoffeeForm"
+import Hero from "./components/Hero"
+import History from "./components/History"
+import Layout from "./components/Layout"
+import Stats from "./components/Stats"
+
+
+function App() {
+
+  const isAuthenticated = false
+
+  const authenticatedContent = (
+    <>
+      <Stats />
+      <History />
+    </>
+  )
+
+  return (
+    <>
+      <Layout>
+        <Hero />
+        <CoffeeForm />
+        {isAuthenticated && (authenticatedContent)}
+      </Layout>
+    </>
+  )
+}
+
+export default App
